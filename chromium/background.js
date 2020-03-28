9823bb99960e91eb0d736d7536d06dc118537bad
doamatto <hello@doamatto.xyz>
2020-03-28 14:15:11 -0700

    Minor edits to README and reorganization of background.js

--- chromium/background.js
+++ chromium/background.js
@@ -1,6 +1,30 @@
-var location0 = window.location.host; // Get tab URL
+function redir () {
+    var location0 = window.location.host; // Get current tab URL
 
-if(location0.includes("amazon.co") || location0.includes("amazon.de")) {
-    var location1 = location0.replace(/www./g,''); // Find 'www.' and replace it with blank text
-    window.location.replace(`https://smile.${location1}`); // Redirect to Smile page
+    var excludeSites = [
+        'advertising.',
+        'affiliate-program.',
+        'alexa.',
+        'amzn_photos_web_us',
+        'aws.',
+        'developer.',
+        'ignite.',
+        'kdp.',
+        'music.',
+        'payments.',
+        'read.',
+        'twitch.',
+        'videodirect.',
+        'acx.',
+        'audible.',
+    ]; // These sites don't support Amazon Smile, but still have '.amazon.com' in them
+
+    if (excludeSites.some(v => location0.includes(v))) {
+        return;
+    } // Checks the excluded subdomain list just in case.
+
+    if(location0.includes("amazon.co") || location0.includes("amazon.de")) {
+        var location1 = location0.replace(/www./g,''); // Find 'www.' and replace it with blank text
+        window.location.replace(`https://smile.${location1}`); // Redirect to Smile page
+    }
 }

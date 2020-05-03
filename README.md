1d64d7857c1169b91cda936fd2602b42391084ca
doamatto <hello@doamatto.xyz>
2020-05-03 00:25:51 -0700

    Massive rewrite with regex and Chrome's WebRequest API

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Always Amazon Smiling is a simple extension to redirect all requests from Amazon
 
 Always Amazon Smiling never saves or shares your URLs. All processing of the Amazon URL is done on your computer and is never sent to me to be sold or collected. I don't want to know that you've bought 52 different Barbie dolls in the past year because why would I waste drive space on that kind of thing when I can waste it on games and code.
 
-This currently only support Chrome and Chromium-based browsers like Brave, but support for Firefox and Safari, as well as being on their first-party stores are coming in the near future.
+This currently only support Chrome and Chromium-based browsers like Brave, but support for Firefox and Safari, as well as being on their first-party stores will appear in the future.
--- chromium/background.js
+++ chromium/background.js
@@ -1,30 +1,57 @@
-function redir () {
-    var location0 = window.location.host; // Get current tab URL
+// These sites don't support Amazon Smile, but still have '.amazon.com' in them
+var excludes = [
+    'advertising.',
+    'affiliate-program.',
+    'alexa.',
+    'amzn_photos_web_us',
+    'aws.',
+    'developer.',
+    'ignite.',
+    'kdp.',
+    'music.',
+    'payments.',
+    'read.',
+    'twitch.',
+    'videodirect.',
+    'acx.',
+    'audible.',
+];
 
-    var excludeSites = [
-        'advertising.',
-        'affiliate-program.',
-        'alexa.',
-        'amzn_photos_web_us',
-        'aws.',
-        'developer.',
-        'ignite.',
-        'kdp.',
-        'music.',
-        'payments.',
-        'read.',
-        'twitch.',
-        'videodirect.',
-        'acx.',
-        'audible.',
-    ]; // These sites don't support Amazon Smile, but still have '.amazon.com' in them
-
-    if (excludeSites.some(v => location0.includes(v))) {
-        return;
-    } // Checks the excluded subdomain list just in case.
-
-    if(location0.includes("amazon.co") || location0.includes("amazon.de")) {
-        var location1 = location0.replace(/www./g,''); // Find 'www.' and replace it with blank text
-        window.location.replace(`https://smile.${location1}`); // Redirect to Smile page
+chrome.webRequest.onBeforeRequest.addListener(function(details){
+    var t;
+    var s = details.url.split('.');
+    s = s.slice(-2).join('.');
+    if(details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1] === "/") {
+        t = "";
+    } else {
+        t = details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1];
     }
-}
+
+    if( details.url.match(excludes) ) { return; } // Ensure the site can support the redirect
+    return {
+
+        // The first regexp will solve for the domain (amazon.co[.uk]m or amazon.de)
+        // The second regexp will solve for the location (everything after and including the '/')
+
+        redirectUrl: "https://smile." + s + t
+    };
+}, {
+    urls: [ // Sites to run on
+        '*://www.amazon.com/*',
+        '*://www.amazon.co.uk/*',
+        '*://www.amazon.de/*',
+        '*://amazon.com/*',
+        '*://amazon.co.uk/*',
+        '*://amazon.de/*',
+    ],
+    types: [
+        "main_frame",
+        "sub_frame",
+        "stylesheet",
+        "script",
+        "image",
+        "object",
+        "xmlhttprequest",
+        "other"
+      ]
+}, ["blocking"]);
